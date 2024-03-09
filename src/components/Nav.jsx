8910c41706ca72345c35f4162979a78fb53ce5d1
Shatlykch <157189400+Shatlykch@users.noreply.github.com>
2024-03-08 19:29:20 -0500

    completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
-import { NavLink } from 'react-router-dom'
-
+import { NavLink } from "react-router-dom"
 
 function Nav() {
   return (
-   <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/Cats">Cats</NavLink>
-    <NavLink to="/add-cat">Add New Cat!</NavLink>
-   </nav>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/cats">Cats</NavLink>
+      <NavLink to="/add-cat">Add New Cat!</NavLink>
+    </nav>
   )
 }
+
 export default Nav
